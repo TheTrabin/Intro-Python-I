8f92a32fbf599ca3239d8f6b88664c9fcddfc9d6
TheTrabin <beyondtears@gmail.com>
2020-09-02 06:49:21 +0000

    4 down

--- src/03_modules.py
+++ src/03_modules.py
@@ -11,21 +11,33 @@ import sys
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
 
+for x in sys.argv:
+    print(x)
+
 # Print out the OS platform you're using:
 # YOUR CODE HERE
-
+print(sys.platform)
 # Print out the version of Python you're using:
 # YOUR CODE HERE
-
+print(sys.version)
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
 # YOUR CODE HERE
-
+print(os.getpid())
 # Print the current working directory (cwd):
 # YOUR CODE HERE
-
+print(os.getcwd())
 # Print out your machine's login name
 # YOUR CODE HERE
+print(os.getlogin())
+
+# /vagrant/Intro-Python-I/src/03_modules.py
+# linux
+# 3.7.9 (default, Aug 18 2020, 06:24:24) 
+# [GCC 5.4.0 20160609]
+# 3277
+# /vagrant/Intro-Python-I
+# vagrant
