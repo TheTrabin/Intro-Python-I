894d8bf5fdeb48663cdbbedec058b038b792f89f
TheTrabin <beyondtears@gmail.com>
2020-09-03 06:48:01 +0000

    Functions Done

--- src/10_functions.py
+++ src/10_functions.py
@@ -2,11 +2,24 @@
 
 # YOUR CODE HERE
 
+def is_even(x):
+    if x % 2 == 0:
+        return True 
+    else:
+        return False
+       
+
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
-
+is_even(num)
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
+def shout(x):
+    if is_even(x) == True:
+        print("Even!")
+    else:
+        print("Odd")
 
+shout(num)
